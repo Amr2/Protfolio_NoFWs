9c25d5070f5b31e2cc178bcbb841c3408eb441da
Amr2 <amr12149@yahoo.com>
2021-07-12 12:35:36 +0200

    add theme changer btn

--- Js/Check-view.js
+++ Js/Check-view.js
@@ -1,15 +1,39 @@
 let logo           = document.querySelector("#logo");
 let sec_log        = document.getElementById("top-logo"); 
-const view_check   = window.matchMedia("(max-width: 500px)") 
+let theme_changer  = document.getElementById("theme"); 
+
+const view_check   = window.matchMedia("(max-width: 500px)");
+
+
+theme_changer.addEventListener("click",()=>{
+
+    document.body.classList.toggle("light");
+    for (const elem of document.querySelectorAll(".logo-img")) {
+        if(elem.src.split("/")[4][0]=="W"){
+            elem.src= "./assets/D-logo-AM-512.svg";
+        }
+        else{
+            elem.src= "./assets/W-logo-AM-512.svg";
+        }
+    }
+});
+
 
 const update_view = (view)=>{
     if(view.matches){
         menu.style.width = "100%";
+        for(let tempelem of document.getElementById("menu").children){
+            tempelem.addEventListener("click", ()=> {
+                menu.style.left  = "-100%";
+                menubtn.style.opacity = "100%"
+                menubtn.style.visibility = "visible";
+            });
+        }
     }
     else{
         menu.style.width = "35%";
     }
-}
+};
 
 logo_obv = new IntersectionObserver((elem)=>{
     
@@ -27,7 +51,7 @@ logo_obv = new IntersectionObserver((elem)=>{
             sec_log.style.width   = "64px";
         },10) ;
     }
-})
+});
 
 update_view(view_check);
 view_check.addListener(update_view);
